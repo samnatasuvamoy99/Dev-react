24861202550a4f71ad8efeb20769fe771c9d8749
suvamoy samanta <suvamoysamanta907@gmail.com>
2025-03-27 02:16:19 +0530

    react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1> oo today i learning react.js</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+   <h1> suvamoy samnata with vite </h1>
+  )
+}
+
+export default App
