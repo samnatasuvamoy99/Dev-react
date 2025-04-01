45fde9d42a9f06dc78183d88bff30f44c2c3f235
suvamoy samanta <suvamoysamanta907@gmail.com>
2025-04-01 14:22:02 +0530

    create your own react library

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,21 +1,34 @@
  //injeact
+ 
    function customRender(reactElement , container){
+    /*
       const domElement = document.createElement( reactElement.type)
       domElement.innerHTML=reactelement.children
       domElement.setAttribute('href' ,reactElement.props.herf)
       document.setAttribute('target' , reactElement.props.target)
        
       container.appenChild(domElement)
+   
+      */
+  // loop base code 
+    const domElement =document.createElement(reactElement.type) 
+     domElement.innerHTML=reactElement.children
+      for( const prop in reactElement.props){
+           if(prop === 'children') continue;
+
+           domElement.setAttribute(prop,reactElement.props[prop])
+      } 
+      container.appendChild(domElement)
    }
- 
+
  const reactelement ={
    type:'a',
      props:{
            href:'https://google.com',
            target:'_blank'
+           
      }
-     ,
-     children:'click me to vist google'
+      ,children:'click me to vist google'
  }
 
 const maincontainer = document.querySelector('#root')
