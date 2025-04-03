23cc16558de6bb09d0a6c9a8b2d545e5fdcc3894
suvamoy samanta <suvamoysamanta907@gmail.com>
2025-04-03 23:44:55 +0530

    password pro

---
+++ 05passwordgen/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className=' text-center text-white w-79 font-bold '> password</h1>
+    </>
+  )
+}
+
+export default App
